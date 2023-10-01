e3a099f87474091ea27db7a238767ba360d72d46
Shivam Dubey <141709786+shivamdubey7870@users.noreply.github.com>
2023-10-01 19:19:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript for adding scrolling effect and changing the menu style
+
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
